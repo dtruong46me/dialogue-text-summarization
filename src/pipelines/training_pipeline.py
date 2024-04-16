db0ef6da4a101c2c011806eae98a2b3957514a48
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-16 11:57:12 +0700

    add: training_pipeline.py

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,55 @@
+import logging
+
+import os
+import sys
+import argparse
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, path)
+
+from utils import *
+
+from model.flant5 import load_model
+from data.preprocessing import preprocessing_data
+from data.ingest_data import ingest_data
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def training_pipeline(args: argparse.Namespace):
+    try:
+        # Load model from checkpoint
+        model = load_model(argparse.checkpoint)
+        logger.info("Complete loading model!")
+
+        # Load data from datapath
+        data = ingest_data(args.datapath)
+        logger.info("Complete loading dataset!")
+
+        # Pre-processing data
+        data = preprocessing_data(data, model.tokenizer)
+        logger.info("Complete pre-processing dataset!")
+
+        # Load training arguments
+        training_args = load_training_arguments(args)
+        logger.info("Complete loading training arguments!")
+
+        # Load trainer
+        trainer = load_trainer(model=model.t5models,
+                               training_args=training_args,
+                               dataset=data,
+                               tokenizer=model.tokenizer)
+        logger.info("Complete loading trainer!")
+
+        # Train model
+        trainer.train()
+        logger.info("Complete training!")
+
+        # Push to Huggingface Hub
+        trainer.push_to_hub()
+        logger.info("Complete pushing model to hub!")
+
+    except Exception as e:
+        logger.error("Error while training: {e}")
+        raise e
+    
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+import argparse
+
+def load_training_arguments(args):
+    return
+
+def load_trainer(args):
+    return
