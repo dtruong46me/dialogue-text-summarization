9bae35e143a94e8dcb6d420bc287db4e734d960b
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-16 13:44:35 +0700

    hotfix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ echo "Xin chao anh em!"
 echo "Chung toi dang setup moi truong, vui long ko phai cho"
 echo "..."
 pip install -U datasets
-pip install requirements.txt
+pip install -r requirements.txt
 echo "---------"
 echo "Set up complete!"
