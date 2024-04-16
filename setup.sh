04b97e83ee21a98dfe4edc09994f88bf572f54c2
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-16 13:50:57 +0700

    fixbug setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+import os
+
 echo "Xin chao anh em!"
 echo "Chung toi dang setup moi truong, vui long ko phai cho"
 echo "..."
 pip install -U datasets
-pip install -r requirements.txt
+pip install -r os.path.join(os.getcwd(), "requirements.txt")
 echo "---------"
 echo "Set up complete!"
