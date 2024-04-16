b76f28a90e47cfa3c4e2fd88a5d719df4f73adc7
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-16 13:58:42 +0700

    final fix bug setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo "Xin chao anh em!"
 echo "Chung toi dang setup moi truong, vui long ko phai cho"
 echo "..."
 pip install -U datasets
-pip install -U -e
+pip install -r "/kaggle/working/dialogue-text-summarization/requirements.txt"
+pip install -r "/content/dialogue-text-summarization/requirements.txt"
 echo "---------"
 echo "Set up complete!"
