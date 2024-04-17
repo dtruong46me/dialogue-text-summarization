8d913836735b0745b77658bc776f1783a1b05bfc
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-17 11:53:14 +0700

    update setup and evaluate

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "Xin chao anh em!"
 echo "Chung toi dang setup moi truong, vui long ko phai cho"
 echo "..."
+pip install --upgrade pip
 pip install -U datasets
 pip install -r "/kaggle/working/dialogue-text-summarization/requirements.txt"
 pip install -r "/content/dialogue-text-summarization/requirements.txt"
--- src/evaluate/evaluation.py
+++ src/evaluate/evaluation.py
@@ -3,13 +3,33 @@ import logging
 import os
 import sys
 
+from datasets import Dataset
+
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, path)
 
 from model.models import load_model
+from data.ingest_data import ingest_data
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def evaluate(data_test: Dataset, model, tokenizer):
+    human_summary = []
+    model_summary = []
+
+    for dialogue in dialogues:
+        prompt = """Summarize the following conversation:\n{dialogue}\nSummary: """
+
+        
+
 if __name__=='__main__':
-    pass
+    datapath = "knkarthick/dialogsum"
+    data = ingest_data(datapath)
+
+    dialogues = data["test"][0:10]["dialogue"]
+    summaries = data["test"][0:10]["summary"]
+
+    print(dialogues)
+    print(type(dialogues))
+
