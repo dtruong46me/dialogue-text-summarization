d05f8c06ccbf73173af4f87750c60e10a62e6b06
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-05-22 16:18:58 +0700

    fix bug

--- src/data/data_strategy.py
+++ src/data/data_strategy.py
@@ -58,7 +58,7 @@ class DataTokenizingStrategy(DataStrategy):
         suffix = "\n###\nSummary: "
         inputs = [prefix + input + suffix for input in data["dialogue"]]
 
-        max_source_length = 1024
+        max_source_length = 512
         max_target_length = 256
 
         data["input_ids"] = self.tokenizer(inputs, max_length=max_source_length, padding="max_length", truncation=True, return_tensors="pt").input_ids
