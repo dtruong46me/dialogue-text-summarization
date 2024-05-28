7718c21e201003d4e65afe21a9deef6d1c6516f1
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-05-28 17:16:15 +0700

    fix bug

--- src/model/models.py
+++ src/model/models.py
@@ -55,7 +55,7 @@ class BartSumModel(GeneralModel):
     def setup(self):
         print("self.base_model = AutoModelForSeq2SeqLM.from_pretrained(self.checkpoint).to(self.device)")
         self.base_model = AutoModelForSeq2SeqLM.from_pretrained(self.checkpoint).to(self.device)
-        self.base_model.generation_config.decoder_start_token_id = self.tokenizer.cls_token_id
+        self.base_model.generation_config.decoder_start_token_id = 2
 
 
 def load_model(checkpoint):
