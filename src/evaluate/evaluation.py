2563791d6c75df42bea48a86499af657b3437a64
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-06-07 21:51:46 +0700

    fix bug

--- src/evaluate/evaluation.py
+++ src/evaluate/evaluation.py
@@ -29,6 +29,7 @@ class RougeEvaluation:
 def evaluation_rouge(model: Model, data: Dataset, generation_config) -> dict:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.base_model = model.get_model()
+    model.base_model.to(device)
 
     dialogues = data["dialogue"]
 
