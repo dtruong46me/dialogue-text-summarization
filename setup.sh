e21d79c1926919d0a61233070f6e8022e468342d
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-06-15 18:43:45 +0700

    fix bug

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
-echo "Xin chao anh em!"
-echo "Chung toi dang setup moi truong ..."
+echo "Hello"
 echo "..."
 pip install -q --upgrade pip
 pip install -q -U datasets
-pip install -q transformers trl
+pip install -q transformers
 pip install -q -r "/kaggle/working/dialogue-text-summarization/requirements.txt"
-pip install -q -r "/content/dialogue-text-summarization/requirements.txt"
-pip install -q -r "requirements.txt"
 echo "---------"
 echo "Set up complete!"
